36e67222c7f75c595e9a6396ecee6dcd73f2e81f
KunalSingh5431 <kunalsingh5431@gmail.com>
2025-11-19 16:42:53 +0530

    Authentication Full Done

--- models/User.js
+++ models/User.js
@@ -1,30 +1,34 @@
-import mongoose  from "mongoose";
-import validator from "validator"
+import mongoose from "mongoose";
+import validator from "validator";
 
-const userSchema =  new mongoose.Schema({
-
-    FullName: {
-   type : String,
-    required: true,
-    minLength : 3,
-    maxLength : 20
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 3,
+      maxLength: 50,
     },
 
     emailId: {
-   type : String,
-    required: true,
-    lowercase :true,
-    trim : true,
-    unique : true,
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: "Invalid email",
+      },
     },
 
     password: {
-     type : String,
-     require: true,
-}, 
-    
-
-    
-},{timestamps:true,});
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("User", userSchema);
